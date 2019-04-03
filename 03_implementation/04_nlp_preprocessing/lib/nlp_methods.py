35c13bbeb710c617f5a2a6b2ad3b12d5db8bdab8
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-03 14:19:41 +0200

    implement remove stopwords to nlp_methods

--- 03_implementation/04_nlp_preprocessing/lib/nlp_methods.py
+++ 03_implementation/04_nlp_preprocessing/lib/nlp_methods.py
@@ -1,5 +1,6 @@
 import re
 import numpy as np
+from nltk.stem.snowball import SnowballStemmer
 
 
 def normalize(sentence):
@@ -13,6 +14,32 @@ def normalize(sentence):
     return sentence.strip()
 
 def normalize_doc(doc):
-    '''Normalizes a list of strings by removing all characters that are not
-    in the swiss alphabet.'''
+    '''
+        Normalizes a list of strings by removing all characters that are
+        not in the swiss alphabet.
+    '''
     return list(map(lambda sentence: normalize(sentence), doc))
+
+
+def tokenize_doc(doc):
+    '''
+        Tokenize a list of strings by spliting words by space character.
+    '''
+    return [sentence.split(' ') for sentence in doc]
+
+
+def remove_stopwords(tokens, stop_words):
+    return [token for token in tokens if token not in stop_words]
+
+def remove_stopwords_doc(token_doc, stopwords):
+    '''
+        Remove stopwords in a list of strings.
+    '''
+    return list(map(lambda t: remove_stopwords(t, stopwords), token_doc))
+
+
+def stemm(tokens, stemmer=SnowballStemmer('german', ignore_stopwords=True)):
+    return [stemmer.stem(token) for token in tokens]
+
+def stemm_doc(token_doc):
+    return list(map(lambda t: stemm(t), token_doc))
