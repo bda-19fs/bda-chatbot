20a38f5810baf0a9f769aa907a31f8b9c61c3b79
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-03 14:21:21 +0200

    use remove stopwords in pipeline

--- 03_implementation/04_nlp_preprocessing/nlp_pipeline.py
+++ 03_implementation/04_nlp_preprocessing/nlp_pipeline.py
@@ -33,24 +33,38 @@ def cli():
 )
 def extract(file, col, out, enc, sep):
     click.echo(f'extracting column {col} from {file}...\n')
-    head, line_numbers = csv_handler.extract(file, col, out, enc, sep)
 
-    click.echo(f'showing head of column {col}:')
-    for i, value in enumerate(head):
-        click.echo(f'[{i}]\t{value}')
-    click.echo(f'\nextracted {line_numbers} of lines -> {out}')
+    values = csv_handler.extract(file, col, out, enc, sep)
+    pickle_handler.dump(values, out)
+
+    click.echo(f'\nextracted {len(values)} of lines -> {out}')
     click.echo('extraction complete!\n')
 
 
-@cli.command('pipeline')
+@cli.command('pipeline_stemming')
 @click.option(
     '-f', '--file', type=click.STRING, default='extract.p',
     help='extract file to use in process (default is "extract.p")'
 )
-def pipeline(file):
+@click.option(
+    '-o', '--out', type=click.STRING, default='pipeline_stemming.p',
+    help='output file for pipeline (default is "pipeline_stemming.p")'
+)
+@click.option(
+    '-s', '--stopwords', type=click.STRING, default='res/custom_ch_stopwords.p',
+    help='stopwords file in pipeline (default is "res/custom_ch_stopwords.p")'
+)
+def pipeline(file, out, stopwords):
     doc = pickle_handler.load(file)
     normalized_doc = nlp_methods.normalize_doc(doc)
-    print(normalized_doc)
+    click.echo(f'- normalized {len(normalized_doc)} sentences')
+
+    tokenized_doc = nlp_methods.tokenize_doc(normalized_doc)
+    click.echo(f'- tokenized {len(tokenized_doc)} sentences')
+
+    stopwords = pickle_handler.load(stopwords)
+    cleaned_doc = nlp_methods.remove_stopwords_doc(tokenized_doc, stopwords)
+    click.echo(f'- removed stopwords from {len(cleaned_doc)} sentences')
 
 
 if __name__ == '__main__':
