f012004951dafa1d2eac8ffe80053788db4f8611
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-03 14:20:43 +0200

    remove pickle import from csv handler

--- 03_implementation/04_nlp_preprocessing/lib/csv_handler.py
+++ 03_implementation/04_nlp_preprocessing/lib/csv_handler.py
@@ -1,9 +1,6 @@
 import pandas as pd
-import pickle
+
 
 def extract(file, col, out, enc, sep):
-    corpus = pd.read_csv(file, sep=sep, encoding=enc, skiprows=[0], usecols=[col])
-    values = corpus.values.flatten().tolist()
-    pickle.dump(values, open(out, 'wb'))
-
-    return (values[:5], len(values))
+    corpus = pd.read_csv(file, sep=sep, encoding=enc, usecols=[col])
+    return corpus.values.flatten().tolist()
