b1729ee8b7dc9acd4d207084e57b1ad27075a762
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-04 09:48:46 +0200

    remove pandas from csv_handler

--- 03_implementation/04_nlp_preprocessing/lib/csv_handler.py
+++ 03_implementation/04_nlp_preprocessing/lib/csv_handler.py
@@ -1,6 +1,3 @@
-import pandas as pd
-
-
-def extract(file, col, out, enc, sep):
-    corpus = pd.read_csv(file, sep=sep, encoding=enc, usecols=[col])
-    return corpus.values.flatten().tolist()
+def csv_iterator(csv, column, seperator):
+    for line in csv.split('\n'):
+        yield '' if line == '' else line.split(seperator)[column]
