55993802214adaa9638569fd41faac4211db10d0
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-04 11:28:27 +0200

    feature - normalizes stdin stream by removing characters not in swiss alphabet

---
+++ 03_implementation/04_nlp_preprocessing/normalize.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import click
+import time as watch
+from lib.log_handler import log_info
+from lib.nlp_methods import normalization_iterator
+
+
+@click.command()
+def normalize():
+    start = watch.time()
+    doc = click.get_text_stream('stdin', 'utf-8').read()
+
+    log_info(f'normalizing')
+
+    lines = 0
+    for line in normalization_iterator(doc):
+        if line is None:
+            break
+        click.get_text_stream('stdout', 'utf-8').write(line + '\n')
+        lines += 1
+
+    log_info(f'normalized {lines} lines')
+
+
+if __name__ == '__main__':
+    normalize()
