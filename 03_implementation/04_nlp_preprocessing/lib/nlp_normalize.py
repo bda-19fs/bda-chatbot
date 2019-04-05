229522942f55151822d7ef3187cdef6b180a8578
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-05 08:22:47 +0200

    extract nlp normalize to nlp_normalize.py

---
+++ 03_implementation/04_nlp_preprocessing/lib/nlp_normalize.py
@@ -0,0 +1,26 @@
+import re
+
+
+def normalize(sentence):
+    double_spaces = r' +'
+    not_in_swiss_alphabet = r'[^\u00C0-\u017Fa-zA-Z\s]'
+
+    sentence = re.sub(not_in_swiss_alphabet, ' ', sentence, re.I | re.A)
+    sentence = re.sub(double_spaces, ' ', sentence, re.I | re.A)
+
+    sentence = sentence.lower()
+    return sentence.strip()
+
+def nlp_normalize(doc):
+    '''
+        Normalizes a stdin stream by removing all characters that are
+        not in the swiss alphabet.
+    '''
+    doc = list(filter(lambda x: x != '', doc.split('\n')))
+
+    lines = 0
+    for line in doc:
+        normalized_line = normalize(line)
+        print(normalized_line)
+        lines += 1
+    return lines
