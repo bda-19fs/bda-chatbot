6e98d9f8b829b0c2e4d71f8bae5a89795283c484
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-05 08:24:50 +0200

    remove nlp normalize methods

--- 03_implementation/04_nlp_preprocessing/lib/nlp_methods.py
+++ 03_implementation/04_nlp_preprocessing/lib/nlp_methods.py
@@ -3,25 +3,6 @@ import numpy as np
 from nltk.stem.snowball import SnowballStemmer
 
 
-def normalize(sentence):
-    double_spaces = r' +'
-    not_in_swiss_alphabet = r'[^\u00C0-\u017Fa-zA-Z\s]'
-
-    sentence = re.sub(not_in_swiss_alphabet, ' ', sentence, re.I | re.A)
-    sentence = re.sub(double_spaces, ' ', sentence, re.I | re.A)
-
-    sentence = sentence.lower()
-    return sentence.strip()
-
-def normalization_iterator(doc):
-    '''
-        Normalizes a stdin stream by removing all characters that are
-        not in the swiss alphabet.
-    '''
-    for line in doc.split('\n'):
-        yield None if line == '' else normalize(line)
-
-
 def convert_to_list(stopwords):
     with open(stopwords, 'r', encoding='utf-8') as file:
         return list(map(lambda line: line.strip(), file.readlines()))
