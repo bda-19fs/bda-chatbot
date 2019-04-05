4cf35613c2323fb4298a5644fec4c1b439196e6e
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-05 08:30:42 +0200

    remove nlp stopword methods

--- 03_implementation/04_nlp_preprocessing/lib/nlp_methods.py
+++ 03_implementation/04_nlp_preprocessing/lib/nlp_methods.py
@@ -3,24 +3,6 @@ import numpy as np
 from nltk.stem.snowball import SnowballStemmer
 
 
-def convert_to_list(stopwords):
-    with open(stopwords, 'r', encoding='utf-8') as file:
-        return list(map(lambda line: line.strip(), file.readlines()))
-
-def remove_stopwords(tokens, stop_words):
-    return [token for token in tokens if token not in stop_words]
-
-def stopwords_iterator(doc, stopwords):
-    '''
-        Remove stopwords in a list of strings.
-    '''
-    stopwords = convert_to_list(stopwords)
-
-    for line in doc.split('\n'):
-        words = line.split(' ')
-        yield None if line == '' else str.join(' ', [word for word in words if word not in stopwords])
-
-
 def stemm(tokens, stemmer=SnowballStemmer('german', ignore_stopwords=True)):
     return [stemmer.stem(token) for token in tokens]
 
