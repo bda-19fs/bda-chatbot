40572ca59bd055dcf27138736c7cbb28cb74d221
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-06 08:43:32 +0200

    move file_as_dict and file_as_list function to extractor.py

--- 03_implementation/04_nlp_preprocessing/lib/extractor.py
+++ 03_implementation/04_nlp_preprocessing/lib/extractor.py
@@ -9,3 +9,17 @@ def extractor(csv, column, separator):
         extracted_column = line.split(separator)[column]
         click.get_text_stream('stdout', 'utf-8').write(extracted_column + '\n')
     return lines
+
+def file_as_dict(file):
+    file_dict = dict()
+
+    with open(file, 'r', encoding='utf-8') as file:
+        for line in file:
+            pair = line.split(',')
+            file_dict[pair[0]] = pair[1].strip()
+
+    return file_dict
+
+def file_as_list(file):
+    with open(file, 'r', encoding='utf-8') as file:
+        return list(map(lambda line: line.strip(), file.readlines()))
