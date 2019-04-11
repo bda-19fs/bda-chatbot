d989a71848611af24d332d3d40be22d9e6fb0374
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-11 19:09:41 +0200

    rename function to normalize_doc_stream

--- bda_chatbot/use_cases/nlp/normalize_doc.py
+++ bda_chatbot/use_cases/nlp/normalize_doc.py
@@ -6,7 +6,7 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from entities.nlp.normalize import normalize
 
 
-def normalize_doc(doc):
+def normalize_doc_stream(doc):
     '''
         Normalizes a stdin stream by removing all characters that are
         not in the swiss alphabet.
@@ -19,3 +19,11 @@ def normalize_doc(doc):
         click.get_text_stream('stdout', 'utf-8').write(normalized_line + '\n')
         lines += 1
     return lines
+
+def normalize_doc(doc):
+    '''
+        Normalizes a list by removing all characters that are
+        not in the swiss alphabet.
+    '''
+    doc = list(filter(lambda x: x != '', doc))
+    return list(map(lambda x: normalize(x), doc))
