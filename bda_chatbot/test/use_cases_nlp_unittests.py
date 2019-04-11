0b23edb89711d9702a0ffb612d5305935f366b51
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-11 20:57:27 +0200

    add unittests for use_cases

---
+++ bda_chatbot/test/use_cases_nlp_unittests.py
@@ -0,0 +1,63 @@
+import unittest
+from os import sys, path
+
+# add modules from parent to path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from use_cases.nlp.normalize_doc import normalize_doc
+from use_cases.nlp.correct_grammar import correct_grammar
+from use_cases.nlp.remove_stopwords import remove_stopwords
+from use_cases.nlp.stemm_doc import stemm_doc
+from use_cases.nlp.lemm_doc import lemm_doc
+
+
+class TestNlpMethods(unittest.TestCase):
+    def setUp(self):
+        self.doc = [
+            'ich kann die bilder in übungen nicht bearbeiten.',
+            'lückentxte weg datenn verloren gegangen von pp 1.3.3 von rabatt',
+            'bitte deaktivieren sie dieses gerät. vielen dank.'
+        ]
+
+    def test_normalize_doc(self):
+        normalized_doc = normalize_doc(self.doc)
+        self.assertEqual(normalized_doc, [
+            'ich kann die bilder in übungen nicht bearbeiten',
+            'lückentxte weg datenn verloren gegangen von pp von rabatt',
+            'bitte deaktivieren sie dieses gerät vielen dank'
+        ])
+
+    def test_correct_grammar(self):
+        corrected_doc = correct_grammar(self.doc)
+        self.assertEqual(corrected_doc, [
+            'ich kann die bilder in übungen nicht bearbeiten.',
+            'lückentexte weg daten verloren gegangen von pp 1.3.3 von rabatt',
+            'bitte deaktivieren sie dieses gerät. vielen dank.'
+        ])
+
+    def test_remove_stopwords(self):
+        cleaned_doc = remove_stopwords(self.doc)
+        self.assertEqual(cleaned_doc, [
+            'bilder übungen bearbeiten.',
+            'lückentxte datenn verloren gegangen pp 1.3.3 rabatt',
+            'bitte deaktivieren gerät. dank.'
+        ])
+
+    def test_stemming(self):
+        stemmed_doc = stemm_doc(self.doc)
+        self.assertEqual(stemmed_doc, [
+            'ich kann die bild in ubung nicht bearbeiten.',
+            'luckentxt weg datenn verlor gegang von pp 1.3.3 von rabatt',
+            'bitt deaktivi sie dieses gerat. viel dank.'
+        ])
+
+    def test_lemming(self):
+        lemmed_doc = lemm_doc(self.doc)
+        self.assertEqual(lemmed_doc, [
+            'ich können der bild in übung nicht bearbeiten.',
+            'lückentxte weg datenn verlieren gehen von pp 1.3.3 von rabatt',
+            'bitte deaktivieren ich dies geraten. viel danken.'
+        ])
+
+
+if __name__ == '__main__':
+    unittest.main()
