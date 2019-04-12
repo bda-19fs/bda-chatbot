851dd78206730cdaa4d3a8c1118a1b579997f836
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-12 13:24:03 +0200

    add stemming for english

--- bda_chatbot/use_cases/nlp/stemm_doc.py
+++ bda_chatbot/use_cases/nlp/stemm_doc.py
@@ -1,28 +1,37 @@
 import click
 from os import sys, path
+from nltk.stem.snowball import SnowballStemmer
 
 # add modules from parent to path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from entities.file.reader import file_as_dict
 from entities.nlp.stemming import stemm
 
+stemm_dic = {
+    'de': 'german',
+    'en': 'english'
+}
 
-def stemm_doc_stream(doc):
+def stemm_doc_stream(doc, language):
     '''
         Stemms all words in stdin stream.
     '''
     doc = list(filter(lambda x: x != '', doc.split('\n')))
+    stemmer = SnowballStemmer(stemm_dic[language])
+
+    print(stemm_dic[language])
 
     lines = 0
     for line in doc:
-        stemmed_line = stemm(line)
+        stemmed_line = stemm(line, stemmer)
         click.get_text_stream('stdout', 'utf-8').write(stemmed_line + '\n')
         lines += 1
     return lines
 
-def stemm_doc(doc):
+def stemm_doc(doc, language):
     '''
         Stemms all words in stdin stream.
     '''
     doc = list(filter(lambda x: x != '', doc))
-    return list(map(lambda x: stemm(x), doc))
+    stemmer = SnowballStemmer(stemm_dic[language])
+    return list(map(lambda x: stemm(x, stemmer), doc))
