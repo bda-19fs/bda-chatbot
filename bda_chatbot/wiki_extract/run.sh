51eb0c5b979354bd76f35948bbd3868d278cc0a4
Cyrill Jauner <cyrill.jauner@stud.hslu.ch>
2019-04-14 14:21:46 +0200

    add some more fixes

--- bda_chatbot/wiki_extract/run.sh
+++ bda_chatbot/wiki_extract/run.sh
@@ -1,2 +1,2 @@
 # Extract the necessary articles from the wiki dump
-python3 use_cases/wiki/WikiExtractor.py /wiki_dumps/de_small_dump.xml --filter_by_ids -ac ./de_article_ids_ionesoft.csv --json
+python3 WikiExtractor.py wiki_dumps/de_small_dump.xml -o wiki_dumps --json --filter_by_ids -ac de_article_ids_ionesoft.csv
