cf9bf0fe6a576990446b74efc91296acdc598916
Cyrill Jauner <cyrill.jauner@stud.hslu.ch>
2019-04-14 13:57:05 +0200

    add corrections to shell script

--- bda_chatbot/wiki_extract/run.sh
+++ bda_chatbot/wiki_extract/run.sh
@@ -1,3 +1,2 @@
 # Extract the necessary articles from the wiki dump
-python -m http.server
-#python3 core/use_cases/WikiExtractor.py ./wiki_dumps/de_small_dump.xml --filter_by_ids -ac ./de_article_ids_ionesoft.csv --json
+python3 use_cases/wiki/WikiExtractor.py /wiki_dumps/de_small_dump.xml --filter_by_ids -ac ./de_article_ids_ionesoft.csv --json
