a08d2ca2cce3c9fe21c3a23229cabb2e1c59b35d
Cyrill Jauner <c.jauner@gmail.com>
2019-04-14 11:27:24 +0200

    add shell script to execute the wiki extraction

--- bda_chatbot/wiki_extract/run.sh
+++ bda_chatbot/wiki_extract/run.sh
@@ -1,4 +1,2 @@
-#
 # Extract the necessary articles from the wiki dump
-#
-python3 WikiExtractor.py /home_static/bda/wiki_dumps/dewiki-21032019-pages-articles.xml --filter_by_ids -ac de_article_ids_ionesoft.csv --json
+python3 core/use_cases/WikiExtractor.py ./wiki_dumps/de_small_dump.xml --filter_by_ids -ac ./de_article_ids_ionesoft.csv --json
