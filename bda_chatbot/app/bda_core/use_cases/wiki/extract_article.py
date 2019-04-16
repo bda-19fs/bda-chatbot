e632081b33a6d17261d33ce10a979c0118cd83f8
Cyrill Jauner <cyrill.jauner@stud.hslu.ch>
2019-04-16 10:28:34 +0200

    remove old function

--- bda_chatbot/app/bda_core/use_cases/wiki/extract_article.py
+++ bda_chatbot/app/bda_core/use_cases/wiki/extract_article.py
@@ -2,14 +2,6 @@ import click
 from bda_core.entities.wiki.json_handler import from_str_to_json, dump_json
 
 
-def extract_text_from_article_stream(doc):
-    doc = list(filter(lambda x: x != '', doc.split('\n')))
-
-    for line in doc:
-        json_data = from_str_to_json(line)
-        click.get_text_stream('stdout', 'utf-8').write(dump_json(json_data) + '\n')
-
-
 def extract_json_documents(doc):
     doc = list(filter(lambda x: x != '', doc.split('\n')))
     json_data = [from_str_to_json(line) for line in doc]
