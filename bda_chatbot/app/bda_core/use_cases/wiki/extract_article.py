16f490f999c9274407eefd8383a5ae8638f4371c
Cyrill Jauner <cyrill.jauner@stud.hslu.ch>
2019-04-16 09:30:54 +0200

    add fixes for new file structure

--- bda_chatbot/app/bda_core/use_cases/wiki/extract_article.py
+++ bda_chatbot/app/bda_core/use_cases/wiki/extract_article.py
@@ -1,6 +1,5 @@
 import click
-from entities.file.reader import file_as_list
-from entities.wiki.json_handler import from_str_to_json, dump_json
+from bda_core.entities.wiki.json_handler import from_str_to_json, dump_json
 
 
 def extract_text_from_article_stream(doc):
@@ -15,3 +14,8 @@ def extract_json_documents(doc):
     doc = list(filter(lambda x: x != '', doc.split('\n')))
     json_data = [from_str_to_json(line) for line in doc]
     return json_data
+
+
+def write_articles_out(json_docs):
+    for doc in json_docs:
+        click.get_text_stream('stdout', 'utf-8').write(dump_json(doc) + '\n')
