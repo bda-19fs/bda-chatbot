cc2a2a0264cc07d637f13be7efc5b42294dff5e9
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-17 14:25:16 +0200

    move folders to root project

---
+++ wiki_extract/run.sh
@@ -0,0 +1,4 @@
+# Extract the necessary articles from the wiki dump
+python3 ./WikiExtractor.py wiki_dumps/de_small_dump.xml -o wiki_dumps --json --filter_by_ids -ac de_article_ids_ionesoft.csv
+
+cat wiki_dumps/de_small_articles.raw | python3 ./ArticleExtractor.py
