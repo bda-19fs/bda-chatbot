9d763b7525b34b5b6b4192b5c4dd879b3f805e5d
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-18 20:15:10 +0200

    add get pipelines

--- backend/api.py
+++ backend/api.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 from flask import Flask
+from flask_cors import CORS
+from flask_restful import Resource, Api
+
+
+class Pipeline(Resource):
+    def get(self):
+        return {
+            'Pipelines': [
+                'TF-IDF',
+                'TF-IDF with stemming'
+            ]
+        }
 
 
 app = Flask(__name__)
+cors = CORS(app, supports_credentials=True)
+api = Api(app)
 
-@app.route('/')
-def hello():
-    return 'hello world!'
+api.add_resource(Pipeline, '/')
 
 
 if __name__ == '__main__':
