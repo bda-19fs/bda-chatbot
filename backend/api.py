563a5248460bb13e20ddc4e6ea58875dad65e63c
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-18 11:27:31 +0200

    add backend with hello world demo

---
+++ backend/api.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'hello world!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
