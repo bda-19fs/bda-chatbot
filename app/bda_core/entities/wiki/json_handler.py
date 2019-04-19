41fd54832becd1ff3cb63641a6376fc5780c0f2c
Cyrill Jauner <c.jauner@gmail.com>
2019-04-19 16:05:15 +0200

    add comments

--- app/bda_core/entities/wiki/json_handler.py
+++ app/bda_core/entities/wiki/json_handler.py
@@ -2,8 +2,18 @@ import json
 
 
 def from_str_to_json(line):
+    '''
+    Converts the given json string to a python dict.
+    :param line: The input json string
+    :return: A python dict
+    '''
     return json.loads(line, encoding='utf8')
 
 
 def dump_json(json_doc):
+    '''
+    Converts the given json document (dict) to a json string
+    :param json_doc: The input dict
+    :return: A json string
+    '''
     return json.dumps(json_doc)
