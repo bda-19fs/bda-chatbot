19b0ef61c8c9a92441145721fec6c441de6d3eed
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-21 18:57:55 +0200

    add training.py to create language models

---
+++ app/bda_core/entities/training/vectorizer.py
@@ -0,0 +1,12 @@
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def fit_concepts(concepts):
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(concepts)
+    return X, vectorizer
+
+def transform_knowledge(vectorizer, questions):
+    tfidf = vectorizer.transform(questions)
+    return tfidf
---
+++ app/bda_core/use_cases/training/utils.py
@@ -0,0 +1,10 @@
+from bda_core.entities.training.vectorizer import (
+    fit_concepts,
+    transform_knowledge
+)
+
+
+def create_language_model(concepts, questions):
+    X, vectorizer = fit_concepts(concepts)
+    language_model = transform_knowledge(vectorizer, questions)
+    return language_model
