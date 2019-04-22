16a55c537882b21e09c4c833e43effdff4f1ac6f
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-22 13:47:57 +0200

    add predictions

--- app/bda_core/entities/prediction/similarity.py
+++ app/bda_core/entities/prediction/similarity.py
@@ -4,7 +4,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 def getKey(item):
     return item[1]
 
-def predict_closest(texts, n):
+def predict_closest(X, vectorizer, texts, n):
     probs = cosine_similarity(X, vectorizer.transform(
         texts
     ))
--- app/bda_core/use_cases/prediction/utils.py
+++ app/bda_core/use_cases/prediction/utils.py
@@ -4,6 +4,6 @@ from bda_core.entities.prediction.similarity import predict_closest
 def readable(probs, answers):
     return [f'{p[1][0]}, {answers[p[0]]}' for p in probs]
 
-def predict_n_answers(texts, answers, n=10):
-    probs = predict_closest(texts, n)
+def predict_n_answers(X, vectorizer, texts, answers, n=10):
+    probs = predict_closest(X, vectorizer, texts, n)
     return readable(probs, answers)
