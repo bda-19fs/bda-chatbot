3d7d77511c15e8b7855332a2403d7df7062ec118
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-22 13:47:25 +0200

    change backend to predict questions

--- backend/api.py
+++ backend/api.py
@@ -2,6 +2,8 @@
 import logging
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from joblib import load
+from bda_core.use_cases.prediction.utils import predict_n_answers
 
 
 app = Flask(__name__)
@@ -19,8 +21,16 @@ def list_pipelines():
 @app.route('/api/v1/run', methods=['POST'])
 def run():
     data = request.get_json(force=True)
-    return jsonify(question = data['question'], config = data['config'])
+    language_model = load('models/language_model.joblib')
+    vectorizer = load('models/language_modelvec.joblib')
+    answers = file_as_list('models/stackexchange_answers.txt')
+    questions = [data['question']]
+    result = predict_n_answers(language_model, vectorizer, questions, answers, 10)
+    return jsonify(result=result)
 
+def file_as_list(file):
+    with open(file, 'r', encoding='utf-8') as file:
+        return list(map(lambda line: line.strip(), file.readlines()))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
