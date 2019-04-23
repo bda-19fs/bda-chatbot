8ce26989ebfb9654e20452e511f18ad21a669d75
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-23 10:51:13 +0200

    test function with stdout

--- app/tests/unit/test_file_use_cases.py
+++ app/tests/unit/test_file_use_cases.py
@@ -1,4 +1,9 @@
-from bda_core.use_cases.file.extract_column import extract_column
+import os
+from os import linesep
+from bda_core.use_cases.file.extract_column import (
+    extract_column,
+    extract_column_stream
+)
 
 
 def test_extract_column():
@@ -15,3 +20,13 @@ def test_extract_column():
         'gegangen',
         'gerät.'
     ]
+
+def test_extract_column_stream(capsys):
+    csv = '''ich,kann,die,bilder,in,übungen,nicht,bearbeiten
+        lückentxte,weg,datenn,verloren,gegangen,von,pp,1.3.3,von,rabatt
+        bitte,deaktivieren,sie,dieses,gerät.,vielen,dank.'''
+
+    extract_column_stream(csv, column=4, separator=',')
+
+    captured = capsys.readouterr()
+    assert captured.out == f'in{linesep}gegangen{linesep}gerät.{linesep}'
