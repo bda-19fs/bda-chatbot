bcb9ed4b65f952340345c800f9f75c65d4698e25
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-25 09:21:13 +0200

    update backend api

--- backend/api.py
+++ backend/api.py
@@ -2,7 +2,10 @@
 import logging
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from lib.handler import handle_request
+from bda_core.use_cases.prediction.algorithm import algorithm_strategy
+from lib.config import (
+    ui_config
+)
 
 
 app = Flask(__name__)
@@ -12,17 +15,15 @@ CORS(app, resources=r'/api/*')
 
 @app.route('/api/v1/pipelines/')
 def list_pipelines():
-    return jsonify(pipelines = [
-        'TF-IDF',
-        'TF-IDF with stemming',
-        'TF-IDF with lemming'
-    ])
+    dataset, algorithm, domain_limit = ui_config()
+    return jsonify(dataset=dataset, algorithm=algorithm, domain_limit=domain_limit)
 
 @app.route('/api/v1/run', methods=['POST'])
 def run():
     data = request.get_json(force=True)
-    result = handle_request(data['question'])
-    return jsonify(result=result)
+    algorithm = algorithm_strategy(data['config'])
+    result = algorithm(data['question'])
+    return jsonify(tfidf=result, skip_gram=[])
 
 
 if __name__ == '__main__':
