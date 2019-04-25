ef03a3cbea94ca055988a225ff1a3e4065a54ea9
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-25 09:21:32 +0200

    add test coverage for prediction

--- app/tests/unit/test_prediction_use_cases.py
+++ app/tests/unit/test_prediction_use_cases.py
@@ -1,10 +1,23 @@
+from bda_core.use_cases.prediction.algorithm import (
+    algorithm_strategy,
+    stackexchange_tfidf_100
+)
 from bda_core.use_cases.prediction.utils import (
-    readable,
-    predict_closest
+    readable
 )
 
+
+config_tfidf = {
+    'dataset': 0,
+    'algorithm': 0,
+    'domain_limit': 0
+}
 
 def test_readable():
     answers = ['1993']
     probs = [(0, [1.0])]
     assert readable(probs, answers) == ['1.0, 1993']
+
+def test_algorithm_strategy():
+    algorithm = algorithm_strategy(config_tfidf)
+    assert algorithm is stackexchange_tfidf_100
