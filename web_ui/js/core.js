2e06fa3c8c6c1cddbb3e978863906cee76f7d4d6
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-25 09:20:08 +0200

    update ui to display tfidf and skip-gram answers

--- web_ui/js/core.js
+++ web_ui/js/core.js
@@ -1,10 +1,24 @@
 let ask_question = function() {
+  console.time('ask_question');
   question = document.querySelector('#question').value;
-  ask(question, '').then(data => fill_answers(data['result']));
+  config = load_config();
+  ask(question, config).then(result => {
+    fill_tfidf_answers(result['tfidf']);
+    fill_skipgram_answers(result['skip_gram']);
+  });
+  console.timeEnd('ask_question');
+}
+
+let load_config = function() {
+  return {
+    'dataset': document.querySelector('#dataset').value,
+    'algorithm': document.querySelector('#algorithm').value,
+    'domain_limit': document.querySelector('#domain_limit').value
+  };
 }
 
 let ask = function(question, config) {
-  return postData('//localhost:7001/api/v1/run', {
+  return post_data('//localhost:7001/api/v1/run', {
       question: question,
       config: config
     })
@@ -16,7 +30,7 @@ let pipelines = function() {
     .then(response => response.json());
 }
 
-let postData = function(url = '', data = {}) {
+let post_data = function(url = '', data = {}) {
     return fetch(url, {
         method: 'POST',
         mode: 'cors',
@@ -28,19 +42,35 @@ let postData = function(url = '', data = {}) {
     });
 }
 
-let fill_algorithm = function(values) {
-  let algorithm = document.querySelector('#algorithm');
+let fill_settings = function(id, values) {
+  let select = document.querySelector(`#${id}`);
   values.forEach(function(text, i) {
     var opt = document.createElement('option');
     opt.value = i
     opt.text = text
-    algorithm.add(opt)
+    select.add(opt)
   })
 }
 
-let fill_answers = function(values) {
-  let answers = document.querySelector('#answers');
-  answers.innerHTML = '';
+let fill_tfidf_answers = function(values) {
+  let tfidf_answers = document.querySelector('#tfidf_answers');
+  tfidf_answers.innerHTML = '';
+  values[0].forEach(function(text) {
+    var text = text.split(',')
+    text[1] = text[1].replace('><', ',')
+    text[1] = text[1].replace('<', '')
+    text[1] = text[1].replace('>', '')
+    var li = document.createElement('li');
+    li.classList.add('p-list__item')
+    li.classList.add('is-ticked')
+    li.innerHTML = `${parseFloat(text[0]).toFixed(4)} ${text[1]}`
+    tfidf_answers.append(li)
+  })
+}
+
+let fill_skipgram_answers = function(values) {
+  let skipgram_answers = document.querySelector('#skipgram_answers');
+  skipgram_answers.innerHTML = '';
   values.forEach(function(text) {
     var text = text.split(',')
     text[1] = text[1].replace('><', ',')
@@ -50,9 +80,17 @@ let fill_answers = function(values) {
     li.classList.add('p-list__item')
     li.classList.add('is-ticked')
     li.innerHTML = `${parseFloat(text[0]).toFixed(4)} ${text[1]}`
-    answers.append(li)
+    skipgram_answers.append(li)
   })
 }
 
 // load settings
-pipelines().then(data => fill_algorithm(data['pipelines']));
+pipelines().then(data => {
+  const {dataset, algorithm, domain_limit} = data;
+  fill_settings('dataset', dataset);
+  fill_settings('algorithm', algorithm);
+  fill_settings('domain_limit', domain_limit);
+});
+
+// empty questions
+document.querySelector('#question').value = ''
