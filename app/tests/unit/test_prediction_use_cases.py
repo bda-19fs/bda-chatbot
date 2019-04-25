1cb5fa925327a3955fdd24261bf2fe3e58eb96aa
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-25 16:00:39 +0200

    add tests for wrapper algorithms

--- app/tests/unit/test_prediction_use_cases.py
+++ app/tests/unit/test_prediction_use_cases.py
@@ -1,12 +1,12 @@
 from bda_core.use_cases.prediction.algorithm import (
     algorithm_strategy,
-    stackexchange_tfidf_100,
-    stackexchange_tfidf_95,
-    stackexchange_tfidf_90,
-    stackexchange_w2v_100
+    stackexchange_100,
+    stackexchange_95,
+    stackexchange_90
 )
 from bda_core.use_cases.prediction.utils import (
-    readable
+    readable,
+    readable_w2v
 )
 
 
@@ -15,6 +15,11 @@ def test_readable():
     probs = [(0, [1.0])]
     assert readable(probs, answers) == ['1.0, 1993']
 
+def test_readable_w2v():
+    answers = ['1993']
+    probs = [(0, [[1.0]])]
+    assert readable_w2v(probs, answers) == ['1.0, 1993']
+
 def test_algorithm_strategy_100():
     config_tfidf = {
         'dataset': 0,
@@ -22,7 +27,7 @@ def test_algorithm_strategy_100():
         'domain_limit': 0
     }
     algorithm = algorithm_strategy(config_tfidf)
-    assert algorithm is stackexchange_tfidf_100
+    assert algorithm is stackexchange_100
 
 def test_algorithm_strategy_95():
     config_tfidf = {
@@ -31,7 +36,7 @@ def test_algorithm_strategy_95():
         'domain_limit': 1
     }
     algorithm = algorithm_strategy(config_tfidf)
-    assert algorithm is stackexchange_tfidf_95
+    assert algorithm is stackexchange_95
 
 def test_algorithm_strategy_90():
     config_tfidf = {
@@ -40,13 +45,4 @@ def test_algorithm_strategy_90():
         'domain_limit': 2
     }
     algorithm = algorithm_strategy(config_tfidf)
-    assert algorithm is stackexchange_tfidf_90
-
-def test_algorithm_strategy_w2v_100():
-    config_tfidf = {
-        'dataset': 0,
-        'algorithm': 6,
-        'domain_limit': 0
-    }
-    algorithm = algorithm_strategy(config_tfidf)
-    assert algorithm is stackexchange_w2v_100
+    assert algorithm is stackexchange_90
