c39fb5b5d66d62000b5ff729cf4863a42339eaec
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-30 15:54:24 +0200

    remove newlines from file

--- app/bda_core/entities/nlp/normalize.py
+++ app/bda_core/entities/nlp/normalize.py
@@ -6,6 +6,7 @@ def normalize(sentence):
     not_in_swiss_alphabet = r'[^\u00C0-\u017Fa-zA-Z\s]'
 
     sentence = re.sub(not_in_swiss_alphabet, ' ', sentence, re.I | re.A)
+    sentence = sentence.replace('\n', ' ')
     sentence = re.sub(double_spaces, ' ', sentence, re.I | re.A)
 
     sentence = sentence.lower()
