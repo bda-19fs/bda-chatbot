a9e580f36089dd5bddab8f352e519497036248f8
Alan Meile <alan.meile@stud.hslu.ch>
2019-04-30 15:55:06 +0200

    change training to have an adjustable pipeline order

--- app/training.py
+++ app/training.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 import os
-import json
+
 import click
-import time as watch
 from joblib import dump
-from bda_core.use_cases.log.log_info import log_info
-from bda_core.use_cases.training.model import create_language_model
+
 from bda_core.entities.file.json_handler import from_str_to_json
+from bda_core.entities.file.reader import file_as_list
+from bda_core.use_cases.log.log_info import log_info
+from bda_core.use_cases.nlp.normalize_doc import normalize_doc_stream
+from bda_core.use_cases.nlp.correct_grammar import correct_grammar_stream
+from bda_core.use_cases.nlp.remove_stopwords import remove_stopwords_stream
+from bda_core.use_cases.nlp.stemm_doc import stemm_doc_stream
+from bda_core.use_cases.nlp.lemm_doc import lemm_doc_stream
 
 
 def get_concepts_from_folder(wiki_extracts):
@@ -20,15 +25,13 @@ def get_concepts_from_folder(wiki_extracts):
                     concepts.append(json_concept['text'])
     return concepts
 
-def file_as_list(file):
-    with open(file, 'r', encoding='utf-8') as file:
-        return list(map(lambda line: line.strip(), file.readlines()))
 
 def save(model, vectorizer, model_name, vec_name):
     dump(model, f'{model_name}.joblib')
     dump(vectorizer, f'{vec_name}.joblib')
 
-@click.command()
+
+@click.group(chain=True, invoke_without_command=True)
 @click.option(
     '-w', '--wiki_extracts', type=click.STRING, required=True,
     help='choose wiki extract folder'
@@ -37,27 +40,76 @@ def save(model, vectorizer, model_name, vec_name):
     '-q', '--questions', type=click.STRING, required=True,
     help='choose questions file'
 )
-def training(wiki_extracts, questions):
-    start = watch.time()
+def cli(wiki_extracts, questions):
+    pass
 
-    log_info(f'collecting wiki_extracts from folder {wiki_extracts}')
+
+@cli.resultcallback()
+def process_pipeline(processors, wiki_extracts, questions):
     concepts = get_concepts_from_folder(wiki_extracts)
-    log_info(f'found {len(concepts)} concepts')
+    questions = (line.rstrip('\r\n') for line in file_as_list(questions, local=False))
+    for processor in processors:
+        questions = processor(questions)
+        concepts = processor(concepts)
+    for item in concepts:
+        click.get_text_stream('stdout', 'utf-8').write(f'{item}\n')
 
-    questions = file_as_list(questions)
-    log_info(f'collected {len(questions)} questions')
 
-    log_info(f'creating language model')
-    model_100, vectorizer_100 = create_language_model(concepts, questions, 0)
-    model_95, vectorizer_95 = create_language_model(concepts, questions, 0.05)
-    model_90, vectorizer_90 = create_language_model(concepts, questions, 0.1)
+@cli.command('normalize')
+def normalize():
+    def processor(doc):
+        for line in normalize_doc_stream(doc):
+            yield f'{line}'
+    return processor
 
-    save(model_100, vectorizer_100, 'tfidf_100_model', 'tfidf_100_vectorizer')
-    save(model_95, vectorizer_95, 'tfidf_95_model', 'tfidf_95_vectorizer')
-    save(model_90, vectorizer_90, 'tfidf_90_model', 'tfidf_90_vectorizer')
 
-    log_info(f'training completed in {watch.time() - start}s\n')
+@cli.command('grammar')
+@click.option(
+    '-g', '--grammar', type=click.STRING, default='res/custom_ch_grammar.txt',
+    help='use grammar file to correct words (default is "res/custom_ch_grammar.txt")'
+)
+def stopwords(grammar):
+    def processor(doc):
+        for line in correct_grammar_stream(doc, grammar):
+            yield f'{line}'
+    return processor
+
+
+@cli.command('stopwords')
+@click.option(
+    '-l', '--language', type=click.STRING, default='res/custom_ch_stopwords.txt',
+    help='stopwords file in pipeline (default is "res/custom_ch_stopwords.txt")'
+)
+def stopwords(language):
+    def processor(doc):
+        for line in remove_stopwords_stream(doc, language):
+            yield f'{line}'
+    return processor
+
+
+@cli.command('stemm')
+@click.option(
+    '-l', '--language', type=click.STRING, default='de',
+    help='choose vocabular language (default is "de")'
+)
+def stopwords(language):
+    def processor(doc):
+        for line in stemm_doc_stream(doc, language):
+            yield f'{line}'
+    return processor
+
+
+@cli.command('lemm')
+@click.option(
+    '-l', '--language', type=click.STRING, default='de',
+    help='choose vocabular language (default is "de")'
+)
+def stopwords(language):
+    def processor(doc):
+        for line in lemm_doc_stream(doc, language):
+            yield f'{line}'
+    return processor
 
 
 if __name__ == '__main__':
-    training()
+    cli()
