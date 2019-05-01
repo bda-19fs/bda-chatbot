69e170707bcd2d6e317c1cba424ec4428afc23c0
Alan Meile <alan.meile@stud.hslu.ch>
2019-05-01 10:44:27 +0200

    add return questions

--- app/bda_core/use_cases/prediction/utils.py
+++ app/bda_core/use_cases/prediction/utils.py
@@ -12,11 +12,11 @@ def readable(probs, answers):
 def readable_w2v(probs, answers):
     return [f'{p[1][0][0]}, {answers[p[0]]}' for p in probs]
 
-def predict_n_answers(X, vectorizer, texts, tags, answers, n=10):
+def predict_n_answers(X, vectorizer, texts, tags, answers, questions, n=10):
     probs = predict_closest(X, vectorizer, texts, n)
-    return readable(probs, tags), readable(probs, answers)
+    return readable(probs, tags), readable(probs, answers), readable(probs, questions)
 
-def predict_n_w2v_answers(question, model, question_vectors, tags, answers, n=10):
+def predict_n_w2v_answers(question, model, question_vectors, tags, answers, questions, n=10):
     X = avg_word_vector(model, question.split(' '))
     probs = predict_closest_w2v(X, question_vectors)
-    return readable_w2v(probs[:n], tags), readable_w2v(probs[:n], answers)
+    return readable_w2v(probs[:n], tags), readable_w2v(probs[:n], answers), readable_w2v(probs[:n], questions)
