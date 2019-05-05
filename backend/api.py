301b346aac226fb4b118c9c7f2d4d6b8fd06d2d6
Alan Meile <alan.meile@stud.hslu.ch>
2019-05-05 12:46:20 +0200

    cache model after first load using memoize

--- backend/api.py
+++ backend/api.py
@@ -3,12 +3,20 @@ import pickle
 import logging
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_caching import Cache
 
+
+from gensim.models import Word2Vec
+from bda_core.use_cases.prediction.utils import (
+    predict_n_answers,
+    predict_n_w2v_answers
+)
 from lib.config import ui_config
 from lib.preprocessing import nlp_strategy
-from lib.algorithm import (
+from lib.loader import (
     load_tags_answers,
-    algorithm_strategy
+    get_model_components,
+    load_models
 )
 
 
@@ -17,7 +25,8 @@ logging.getLogger('flask_cors').level = logging.DEBUG
 
 
 app = Flask(__name__)
-CORS(app, resources=r'/api/*')
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+cors = CORS(app, resources=r'/api/*')
 
 
 @app.route('/api/v1/pipelines/')
@@ -32,15 +41,22 @@ def run():
     data = request.get_json(force=True)
     nlp = nlp_strategy(data['config'])
     question = nlp(data['question'])
-    algorithm = algorithm_strategy(data['config'])
     dataset = dataset[int(data['config']['dataset'])].lower()
     tags, answers, questions = load_tags_answers(dataset)
-    tt, ta, tq, wt, wa, wq = algorithm(question, tags, answers, questions)
+    language_model_tfidf, vectors_tfidf, language_model_w2v, vectors_w2v = load_models_with_caching(data['config'])
+    tt, ta, tq = predict_n_answers(language_model_tfidf, vectors_tfidf, [question], tags, answers, questions, 10)
+    wt, wa, wq = predict_n_w2v_answers(question, language_model_w2v, vectors_w2v, tags, answers, questions, 10)
     return jsonify(
         nlp_question=question,
         tfidf_tags=tt, tfidf_answers=ta, tfidf_questions=tq,
         w2v_tags=wt, w2v_answers=wa, w2v_questions=wq
     )
+
+
+@cache.memoize(timeout=0)
+def load_models_with_caching(config):
+    folder, model, vectors = get_model_components(config)
+    return load_models(folder, model, vectors)
 
 
 if __name__ == '__main__':
