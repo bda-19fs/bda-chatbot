b24f9950e91b2e52573d84fd7e6c66690c7667b9
Alan Meile <alan.meile@stud.hslu.ch>
2019-05-15 13:26:11 +0200

    fix type in used script file names

--- wiki_extract/run.sh
+++ wiki_extract/run.sh
@@ -1,4 +1,4 @@
 # Extract the necessary articles from the wiki dump
-python3 ./WikiExtractor.py data/de_small_dump.xml -o data --json --filter_by_ids -ac de_article_ids_ionesoft.csv
+python3 ./wiki_extractor.py data/de_small_dump.xml -o data --json --filter_by_ids -ac de_article_ids_ionesoft.csv
 
-cat data/de_small_articles.raw | python3 ./articleextractor.py
+cat data/de_small_articles.raw | python3 ./article_extractor.py
