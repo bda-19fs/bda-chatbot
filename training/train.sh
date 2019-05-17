ae11c21547c9027dd1832a96d3d3507ae95785d5
Alan Meile <alan.meile@gmail.com>
2019-05-17 10:55:33 +0200

    add new training processes

--- training/train.sh
+++ training/train.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt normalize
+python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt -n "stemming" normalize stemming -l "en"
+python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt -n "lemming" normalize lemming -l "en"
 
