0a8a730379e379b02e66ec88780205384c527dc2
Alan Meile <alan.meile@stud.hslu.ch>
2019-05-17 19:01:36 +0200

    fix parameter for stopwords

--- training/train_stackexchange.sh
+++ training/train_stackexchange.sh
@@ -3,6 +3,6 @@
 python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt normalize
 python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt -n "stemming_" normalize stemm -l "en"
 python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt -n "lemming_" normalize lemm -l "en"
-python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt -n "stopwords_" normalize stopwords -s "res/custom_en_stopwords.txt"
-python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt -n "stopwords_stemming_" normalize stopwords -s "res/custom_en_stopwords.txt" stemm -l "en"
-python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt -n "stopwords_lemming_" normalize stopwords -s "res/custom_en_stopwords.txt" lemm -l "en"
+python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt -n "stopwords_" normalize stopwords -l "res/custom_en_stopwords.txt"
+python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt -n "stopwords_stemming_" normalize stopwords -l "res/custom_en_stopwords.txt" stemm -l "en"
+python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt -n "stopwords_lemming_" normalize stopwords -l "res/custom_en_stopwords.txt" lemm -l "en"
