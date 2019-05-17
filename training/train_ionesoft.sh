ee7ec6d64cd72a9783a93461faaaab2235f60f20
Alan Meile <alan.meile@stud.hslu.ch>
2019-05-17 15:04:38 +0200

    add all training routines

--- training/train_ionesoft.sh
+++ training/train_ionesoft.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt normalize
-python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "stemming_" normalize stemm
-python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "lemming_" normalize lemm
+python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt normalize grammar
+python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "stemming_" normalize grammar stemm
+python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "lemming_" normalize grammar lemm
+python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "stopwords_" normalize grammar stopwords
+python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "stopwords_stemming_" normalize grammar stopwords stemm
+python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "stopwords_lemming_" normalize grammar stopwords lemm
