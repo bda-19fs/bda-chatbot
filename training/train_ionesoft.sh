e4e6fa86aa657f00e13d1662e5a6170d33a1da86
Alan Meile <alan.meile@stud.hslu.ch>
2019-05-17 13:48:25 +0200

    fix typo in train files

--- training/train_ionesoft.sh
+++ training/train_ionesoft.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt normalize
-python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "stemming_" normalize stemming
-python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "lemming_" normalize lemming
+python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "stemming_" normalize stemm
+python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "lemming_" normalize lemm
--- training/train_stackexchange.sh
+++ training/train_stackexchange.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt normalize
-python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt -n "stemming_" normalize stemming -l "en"
-python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt -n "lemming_" normalize lemming -l "en"
+python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt -n "stemming_" normalize stemm -l "en"
+python3 /app/training.py -w /mnt/data/raw/wiki/en/ -q /mnt/data/raw/stackexchange_questions.txt -n "lemming_" normalize lemm -l "en"
