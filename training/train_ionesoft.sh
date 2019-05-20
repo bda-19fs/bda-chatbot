a21edf4f0f59dd644cc84b69115d3aff21e735c2
Alan Meile <alan.meile@stud.hslu.ch>
2019-05-20 10:54:04 +0200

    add w2v training routine

--- training/train_ionesoft.sh
+++ training/train_ionesoft.sh
@@ -6,3 +6,10 @@ python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_que
 python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "stopwords_" normalize grammar stopwords
 python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "stopwords_stemming_" normalize grammar stopwords stemm
 python3 /app/training.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "stopwords_lemming_" normalize grammar stopwords lemm
+
+python3 /app/training_w2v.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt normalize grammar
+python3 /app/training_w2v.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "stemming_" normalize grammar stemm
+python3 /app/training_w2v.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "lemming_" normalize grammar lemm
+python3 /app/training_w2v.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "stopwords_" normalize grammar stopwords
+python3 /app/training_w2v.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "stopwords_stemming_" normalize grammar stopwords stemm
+python3 /app/training_w2v.py -w /mnt/data/raw/wiki/de/ -q /mnt/data/raw/ionesoft_questions.txt -n "stopwords_lemming_" normalize grammar stopwords lemm
