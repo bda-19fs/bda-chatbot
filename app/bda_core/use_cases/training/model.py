f23b6eb61423cc7d66fd386536f161bbe98c0bcc
Cyrill Jauner <cyrill.jauner@stud.hslu.ch>
2019-05-20 09:11:27 +0200

    add matrix generation to the training process

--- app/bda_core/use_cases/training/model.py
+++ app/bda_core/use_cases/training/model.py
@@ -14,4 +14,5 @@ def create_language_model(concepts, questions, domain_limit):
 def create_w2v_model(sentences, questions, config=w2v.Config()):
     model = w2v.fit_model(sentences, config)
     vectors = w2v.create_sentence_vectors(model, questions)
+    vectors = w2v.create_matrix_from_vectors(vectors)
     return model, vectors
