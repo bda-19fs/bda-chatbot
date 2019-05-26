289274022decc068b5e7b7dfa25049701b20956e
Alan Meile <alan.meile@gmail.com>
2019-05-26 11:44:24 +0200

    finalize wiki extract script

--- wiki_extract/wiki_extract.sh
+++ wiki_extract/wiki_extract.sh
@@ -2,9 +2,9 @@
 
 
 python3 /app/wiki_extractor.py /mnt/data/raw/xml/enwiki-latest-pages-articles.xml -o data --json --filter_by_ids -ac /wiki_extract/en_article_ids_stackexchange.csv
-mv -vf /wiki_extract/data/AA /mnt/data/raw/wiki/en
+mv -vf /wiki_extract/data/AA/* /mnt/data/raw/wiki/en
 rm -rf /wiki_extract/data
 
 python3 /app/wiki_extractor.py /mnt/data/raw/xml/dewiki-21032019-pages-articles.xml -o data --json --filter_by_ids -ac /wiki_extract/de_article_ids_ionesoft.csv
-mv -vf /wiki_extract/data/AA /mnt/data/raw/wiki/de
+mv -vf /wiki_extract/data/AA/* /mnt/data/raw/wiki/de
 rm -rf /wiki_extract/data
